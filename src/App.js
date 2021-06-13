490420634ef79a214eb0365218708d431a27c860
Mirabror Sodikov <mirabror9545@gmail.com>
2021-06-13 21:12:46 +0500

    Users, UsersItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
-function App() {
-  return <h1>Hello React</h1>;
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
