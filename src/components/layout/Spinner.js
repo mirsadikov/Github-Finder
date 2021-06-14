adb990493f517fea9c2ed53c6d58bab51f1b2637
Mirabror Sodikov <mirabror9545@gmail.com>
2021-06-14 20:38:10 +0500

    spinner, github users

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import spinner from './spinner.gif';
+
+export default function Spinner() {
+  return (
+    <Fragment>
+      <img
+        src={spinner}
+        alt='Loading...'
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+      />
+    </Fragment>
+  ); 
+}
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,30 @@
 import React, { Component } from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
 export default class Users extends Component {
-  state = {
-    users: [
-      {
-        login: 'mojombo',
-        id: 1,
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'defunkt',
-        id: 2,
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        login: 'pjhyett',
-        id: 3,
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
-
   render() {
-    return (
-      <div style={userStyle}>
-        {this.state.users.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
+    const { users, loading } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    } else {
+      return (
+        <div style={userStyle}>
+          {this.props.users.map((user) => (
+            <UserItem key={user.id} user={user} />
+          ))}
+        </div>
+      );
+    }
   }
 }
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: 'grid',
