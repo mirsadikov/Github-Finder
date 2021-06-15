926b62e89e810cfd1a5e9c0f25c126ab2626a69f
Mirabror Sodikov <mirabror9545@gmail.com>
2021-06-15 16:00:07 +0500

    repos

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom';
 
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import Repos from '../repos/Repos';
 
 export default class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
 
   render() {
@@ -32,7 +36,7 @@ export default class User extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -56,7 +60,7 @@ export default class User extends Component {
               style={{ width: '150px' }}
             />
             <h1>{name}</h1>
-            <p>Location: {location}</p>
+            <p>Location: {location ? location : 'Hidden'}</p>
           </div>
           <div>
             {bio && (
@@ -102,6 +106,7 @@ export default class User extends Component {
           <div className='badge badge-light'>Public Repos: {public_repos}</div>
           <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
