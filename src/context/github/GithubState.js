b985e274f1d1a6b426e1d6698c55af5c2d1f5814
Mirabror Sodikov <mirabror9545@gmail.com>
2021-06-20 14:53:52 +0500

    env test

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -15,16 +15,16 @@ import {
 // Config
 let githubClientId;
 let githubClientSecret;
-githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+// githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+// githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
 
-// if (process.env.NODE_ENV !== 'production') {
-//   githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-//   githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
-// } else {
-//   githubClientId = process.env.GITHUB_CLIENT_ID;
-//   githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
-// }
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
 
 const GithubState = (props) => {
   const initialState = {
